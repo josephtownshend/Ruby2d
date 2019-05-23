27d724efc23f18c7f39c5db5bd6320a5c2345c1f
joseph townshend <joetownshend@mac.com>
2019-05-23 12:36:58 +0100

    left_player movement test

--- pong.rb
+++ pong.rb
@@ -9,12 +9,26 @@ net = Rectangle.new(
   z: 0
 )
 
-left_player = Rectangle.new(
+@left_player = Rectangle.new(
   x: 20, y: (Window.height / 2)-40,
   width: 15, height: 80,
   color: 'white',
   z: 0
 )
+
+@y_speed = 0
+
+on :key_down do |event|
+  if event.key == 'w'
+    @y_speed = -2
+  elsif event.key == 's'
+    @y_speed = 2
+  end
+end
+
+update do
+  @left_player.y += @y_speed
+end
 
 right_player = Rectangle.new(
   x: Window.width - 35, y: (Window.height / 2)-40,
