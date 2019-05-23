cb2031930fabdf9df847b0d90ab4b9665ec98576
Ben Scott <bengscott2@gmail.com>
2019-05-23 12:49:00 +0100

    Added right player movement

--- pong.rb
+++ pong.rb
@@ -16,26 +16,17 @@ net = Rectangle.new(
   z: 0
 )
 
-@y_speed = 0
+@y_speed_left_player = 0
 
-on :key_down do |event|
-  if event.key == 'w'
-    @y_speed = -2
-  elsif event.key == 's'
-    @y_speed = 2
-  end
-end
 
-update do
-  @left_player.y += @y_speed
-end
-
-right_player = Rectangle.new(
+@right_player = Rectangle.new(
   x: Window.width - 35, y: (Window.height / 2)-40,
   width: 15, height: 80,
   color: 'white',
   z: 0
 )
+
+@y_speed_right_player = 0
 
 ball = Circle.new(
   x: (Window.width / 2), y: 15,
@@ -44,4 +35,32 @@ ball = Circle.new(
   color: 'green',
   z: 10
 )
+
+
+on :key_down do |event|
+  if event.key == 'w'
+    @y_speed_left_player = -2
+  elsif event.key == 's'
+    @y_speed_left_player = 2
+  elsif event.key == 'i'
+    @y_speed_right_player = -2
+  elsif event.key == 'k'
+    @y_speed_right_player = 2
+  end
+end
+
+update do
+  @left_player.y += @y_speed_left_player
+  @right_player.y += @y_speed_right_player
+
+  if @left_player.y < 0 || @left_player.y > Window.height - 80
+    @y_speed_left_player = 0
+  end
+
+  if @right_player.y < 0 || @right_player.y > Window.height - 80
+    @y_speed_right_player = 0
+  end
+
+end
+
 show
